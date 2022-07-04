4dd4f2eda0772a33c67df9db9ed1cae3e76966c4
Boren Xue <53597966+borenx1@users.noreply.github.com>
2022-07-04 23:09:04 +1200

    Update placeholder home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+  <title>Maths Games</title>
+</svelte:head>
+
+<h1 class="text-3xl font-bold">Welcome to Maths Games</h1>
+<p>We have maths games and resources to help you learn maths!</p>
+<p>Coming soon™</p>
